43ef3c3832dc2ab691d377efab1fc99f21d25418
EricksonLopes <ofc.erickson@gmail.com>
2020-07-05 21:57:15 -0300

    search to filter by user

--- agenda/core/models.py
+++ agenda/core/models.py
@@ -1,10 +1,21 @@
 from django.db import models
+# usuario logado
+from django.contrib.auth.models import User
 
 
+# makemigrations core
 class Evento(models.Model):
-    titulo = models.CharField(max_length=100)
+    titulo = models.CharField(max_length=100, verbose_name='Título')
     descricao = models.TextField(blank=True, null=True)
-    data_evento = models.DateTimeField()
-    data_criacao = models.DateTimeField(auto_now=True)
+    data_evento = models.DateTimeField(verbose_name='Data do Evento')
+    data_criacao = models.DateTimeField(auto_now=True, verbose_name='DAta da Criação')
+    # se o usuario for excluido os dados dele são deletados
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 
+    class Meta:
+        db_table = 'evento'
 
+    def __str__(self):
+        return self.titulo
+
+# migrate
